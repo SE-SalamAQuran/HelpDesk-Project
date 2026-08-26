d61f28815415996c4eaa4c576fd75253ef08d2c5
Omar._alqaisi06 <omar.qaisi735@gmail.com>
2026-08-26 12:01:49 +0300

    add Firestore setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 import os
 import firebase_admin
-from firebase_admin import credentials
+from firebase_admin import credentials, firestore
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -13,3 +13,5 @@ firebase_key = os.path.join(
 if not firebase_admin._apps:
     cred = credentials.Certificate(firebase_key)
     firebase_admin.initialize_app(cred)
+
+db = firestore.client()
