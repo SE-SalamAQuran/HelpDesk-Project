e4406e5832d084efdacd90513d8634b867921141
Omar._alqaisi06 <omar.qaisi735@gmail.com>
2026-08-26 11:32:27 +0300

    firebase added

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+import os
+import firebase_admin
+from firebase_admin import credentials
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+firebase_key = os.path.join(
+    BASE_DIR,
+    "Resources",
+    "serviceAccountKey.json"
+)
+
+if not firebase_admin._apps:
+    cred = credentials.Certificate(firebase_key)
+    firebase_admin.initialize_app(cred)
